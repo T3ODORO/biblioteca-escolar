ab7954a54c87a29cdf9a3222e30b9f031f5ff8e9
T3ODORO <79917308+T3ODORO@users.noreply.github.com>
2024-10-04 07:05:47 -0300

    Initial commit

---
+++ src/main/java/com/biblioteca/escolar/BibliotecaEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/escolar/BibliotecaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
